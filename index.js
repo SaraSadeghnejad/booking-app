f6142106e0c6cf87434ea4abffe43e1114bcf46f
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-03-28 23:59:59 +0330

    add package

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express"
-
+import mongoose  from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
 const app =express();
+const connect = async()=>{
+try {
+  await mongoose.connect('mongodb+srv://sarasadeghnejad33:VRjoxk6yabb6BH0F@cluster0.4se1n0h.mongodb.net/ecommerceData');
+  console.log('connected to mong');
+} catch (error) {
+  throw error;
+}
+}
 
 app.listen(8800,()=>{
+    connect()
     console.log("Connected to the backend")
 })
