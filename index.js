1f87e07e11d5c04d089eca25960ea43665f2eda2
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-03-30 00:51:34 +0330

    add models and controllers and routers

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-import express from "express"
-import mongoose  from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
-const app =express();
-const connect = async()=>{
-try {
-  await mongoose.connect('mongodb+srv://sarasadeghnejad33:VRjoxk6yabb6BH0F@cluster0.4se1n0h.mongodb.net/ecommerceData');
-  console.log('connected to mong');
-} catch (error) {
-  throw error;
-}
-}
-
-app.listen(8800,()=>{
-    connect()
-    console.log("Connected to the backend")
-})
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.js";
+import hotelRouter from "./routes/hotels.js";
+import roomRouter from "./routes/rooms.js";
+import userRouter from "./routes/users.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
+const app = express();
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("connected to mongo");
+  } catch (error) {
+    throw error;
+  }
+};
+mongoose.connection.on("connected", () => {
+  console.log("mongodb connected");
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("mongodb disconnected");
+});
+app.use(cookieParser())
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use("/api/hotels", hotelRouter);
+app.use("/api/rooms", roomRouter);
+app.use("/api/users", userRouter);
+app.use((error, req, res, next) => {
+  const errorStatus = error.status || 500;
+  const errorMessage = error.message || "Hello error from handler";
+  return res
+    .status(errorStatus)
+    .json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack
+    });
+});
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to the backend");
+});
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
+const route = express.Router();
+
+route.get('/checkauthentication',verifyToken,(req,res,next) => {
+  res.send("hello user ,you are logged in")
+})
+route.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("hello user ,you are logged in and you can delete your account");
+});
+route.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("hello admin ,you are logged in and you can delete all accounts");
+});
+//update a route
+route.put("/:id", verifyUser,updateUser);
+//delete a route
+route.delete("/:id", verifyUser,deleteUser);
+route.get("/:id", verifyUser,getUser);
+route.get("/", verifyAdmin,getUsers);
+export default route;
