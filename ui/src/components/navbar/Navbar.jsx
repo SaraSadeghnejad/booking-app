048a1dbf836cd666b4fae1f79dd2a4777e3802d5
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2024-03-30 20:26:19 +0330

    add reserve component and update list and hotel page

--- ui/src/components/navbar/Navbar.jsx
+++ ui/src/components/navbar/Navbar.jsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router-dom"
 import "./navbar.css"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        <span className="logo">
+          <Link to="/">lamabooking</Link>
+        </span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
